18a8fb6ff4e00fb0a56bfa00f4d015998c56e0ea
Charlie <cprincewebdev@gmail.com>
2024-05-05 23:55:44 +0100

    deployment.. wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: __dirname,
+  base: '/character-creator/',
   cacheDir: './node_modules/.vite/.',
   server: {
     port: 4200,
